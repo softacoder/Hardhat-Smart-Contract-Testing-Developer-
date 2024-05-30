e16245f0286ba6ced7a571a04c148a4940003273
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-30 15:16:37 +0100

    commit test

---
+++ contract/MyTest.sol
@@ -0,0 +1,58 @@
+// //SPDX_License-Identifier: UNLICENSED
+// pragma solidity ^0.8.9;
+
+// //IMPORT THE HARDHAT CONSOLE
+// import "hardhat/console.sol";
+
+// contract MyTest{
+
+//     uint256 public unlockedTime;
+//     address payable public owner;
+
+//     event Withdrawal(uint256 amount, uint256 when);
+
+//     constructor(uint256 _unlockedTime) payable{
+//         require(block.timestamp < _unlockedTime, "Unlocked time should be in the future");
+
+//         unlockedTime = _unlockedTime;
+//         owner = payable(msg.sender);
+// }
+
+// function withdraw() public{
+//     require(block.timestamp >= unlockedTime, "Wait till the time period completes");
+//     require(msg.sender == owner, "You are not an owner");
+
+//     emit Withdrawal(address(this).balance, block.timestamp);
+
+//     owner.transfer(address(this).balance);
+// }
+// }
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Import the Hardhat console for debugging
+import "hardhat/console.sol";
+
+contract MyTest {
+    uint256 public unlockedTime;
+    address payable public owner;
+
+    event Withdrawal(uint256 amount, uint256 when);
+
+    constructor(uint256 _unlockedTime) payable {
+        require(block.timestamp < _unlockedTime, "Unlocked time should be in the future");
+
+        unlockedTime = _unlockedTime;
+        owner = payable(msg.sender);
+    }
+
+    function withdraw() public {
+        require(block.timestamp >= unlockedTime, "Wait till the time period completes");
+        require(msg.sender == owner, "You are not the owner");
+
+        emit Withdrawal(address(this).balance, block.timestamp);
+
+        owner.transfer(address(this).balance);
+    }
+}
