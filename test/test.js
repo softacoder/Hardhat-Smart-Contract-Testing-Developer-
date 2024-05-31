2ae7985af12d45772d40ee0283fcd0e137153ccd
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-31 02:32:14 +0100

    commit loadFixture

--- test/test.js
+++ test/test.js
@@ -33,11 +33,18 @@ describe("MyTest", function () {
     // console.log(otherAccount);
 
     const MyTest = await ethers.getContractFactory("MyTest");
-    const myTest = await MyTest.deployed(unlockedTime, { value: lockedAmount });
+    const myTest = await MyTest.deploy(unlockedTime, { value: lockedAmount });
 
     console.log(myTest, unlockedTime, lockedAmount, owner, otherAccount);
 
     return { myTest, unlockedTime, lockedAmount, owner, otherAccount };
   }
+
+  describe("Deployment", function () {
+    it("Should check unlocked time", async function () {
+      const { myTest, unlockedTime } = await loadFixture(runEveryTime);
+    });
+  });
+
   runEveryTime();
 });
