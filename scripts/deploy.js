b592634a0456700ad896ffeea8780425b431cc45
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-31 01:21:51 +0100

    commit debugged parseEthers

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -21,20 +21,21 @@
 // });
 
 const hre = require("hardhat");
+console.log(hre);
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECONDS = 365 * 24 * 60 * 60; // Corrected the days in a year
-  const unlockedTime = currentTimestampInSeconds + ONE_YEAR_IN_SECONDS;
+  const ONE_YEARS_IN_SECONDS = 365 * 24 * 60 * 60; // Corrected the days in a year
+  const unlockedTime = currentTimestampInSeconds + ONE_YEARS_IN_SECONDS;
 
   const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const MyTest = await hre.ethers.getContractFactory("MyTest");
-  const myTest = await MyTest.deploy(unlockedTime, { value: lockedAmount });
+  // const MyTest = await hre.ethers.getContractFactory("MyTest");
+  // const myTest = await MyTest.deploy(unlockedTime, { value: lockedAmount });
 
-  await myTest.deployed();
+  // await myTest.deployed();
 
-  console.log(`MyTest deployed to: ${myTest.address}`);
+  // console.log(lockedAmount);
 }
 
 main().catch((error) => {
