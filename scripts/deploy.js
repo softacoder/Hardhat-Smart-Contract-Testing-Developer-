eb389d46c4daffd1b95843194e69bde060214189
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-31 02:23:05 +0100

    commit commit myTest, unlockedTime, lockedAmount, owner, otherAccount

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,10 @@
-// const hre = require("hardhat");
-// console.log(hre);
-
-// async function main() {
-//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-//   const ONE_YEARS_IN_SECONDS = 356 * 24 * 60 * 60;
-//   const unlockedTime = currentTimestampInSeconds + ONE_YEARS_IN_SECONDS;
-
-//   const lockedAmount = hre.ethers.utils.parseEther("1");
-//   //   const lockedAmount = hre.ethers;
-
-//   //   console.log(currentTimestampInSeconds);
-//   //   console.log(ONE_YEARS_IN_SECONDS);
-//   //   console.log(unlockedTime);
-//   console.log(lockedAmount);
-// }
-
-// main().catch((error) => {
-//   console.log(error);
-//   process.exitCode = 1;
-// });
-
 const hre = require("hardhat");
 console.log(hre);
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEARS_IN_SECONDS = 365 * 24 * 60 * 60; // Corrected the days in a year
-  const unlockedTime = currentTimestampInSeconds + ONE_YEARS_IN_SECONDS;
+  const ONE_YEAR_IN_SECONDS = 365 * 24 * 60 * 60; // Corrected the days in a year
+  const unlockedTime = currentTimestampInSeconds + ONE_YEAR_IN_SECONDS;
 
   const lockedAmount = hre.ethers.utils.parseEther("1");
 
--- test/test.js
+++ test/test.js
@@ -18,9 +18,26 @@ const { ethers } = require("hardhat");
 describe("MyTest", function () {
   async function runEveryTime() {
     const ONE_YEAR_IN_SECONDS = 356 * 24 * 60 * 60;
-    const ONE_GEWI = 1000000000;
+    const ONE_GEWI = 1_000_000_000;
 
-    console.log(ONE_YEAR_IN_SECONDS, ONE_GEWI);
+    const lockedAmount = ONE_GEWI;
+    const unlockedTime = (await time.latest()) + ONE_YEAR_IN_SECONDS;
+
+    // console.log(ONE_YEAR_IN_SECONDS, ONE_GEWI);
+    // console.log(unlockedTime);
+
+    // GET ACCOUNTS
+    const [owner, otherAccount] = await ethers.getSigners();
+    // const [owner, act1, act2, act3] = await ethers.getSigners();
+    // console.log(owner);
+    // console.log(otherAccount);
+
+    const MyTest = await ethers.getContractFactory("MyTest");
+    const myTest = await MyTest.deployed(unlockedTime, { value: lockedAmount });
+
+    console.log(myTest, unlockedTime, lockedAmount, owner, otherAccount);
+
+    return { myTest, unlockedTime, lockedAmount, owner, otherAccount };
   }
   runEveryTime();
 });
